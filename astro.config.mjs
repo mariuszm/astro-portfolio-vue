bc7d58467ca68881155511785723e8eb5b1f0d16
Mariusz Michalski <supermariusz@gmail.com>
2024-06-15 23:48:17 +0200

    chore(deps): added tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [vue({
     script: {
       defineModel: true,
